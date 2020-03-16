5213fb4068a4fefe854cbb3ea761a3a718a14dbc
Win10 <mohammed.s.eltwahy@gmail.com>
2020-03-16 13:56:43 +0200

    hello akhbarr app

---
+++ lib/PageView.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class PgeView extends StatefulWidget {
+  @override
+  _PgeViewState createState() => _PgeViewState();
+}
+
+class _PgeViewState extends State<PgeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    body:Container(
+      color: Colors.red,
+    ),
+    );
+  }
+}
